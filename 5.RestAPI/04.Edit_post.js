d6ab6afdea6835b799c1d5bb4b7995edf7485346
ritik7739 <ritik.kumar7739@gmail.com>
2024-08-01 18:02:07 +0530

    Add all learning of  RESTAPI

--- 5.RestAPI/04.Edit_post.js
+++ 5.RestAPI/04.Edit_post.js
@@ -1,21 +1,23 @@
 const express=require("express");
-const path = require("path");
 const app=express();
 const port=3000;
-const { v4:uuidv4 }=require('uuid');
+const path=require('path');
+const { v4:uuidv4 }=require("uuid");
+const methodOverride = require("method-override");
 
+app.set("view engine","/views");
+app.set("views",path.join(__dirname,"/views"));
+
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-
 let posts=[
-    {   
+    {
         id:uuidv4(),
         username:"ritik",
-        content:"I love coding"
+        content:"I love coding",
     },
     {
         id:uuidv4(),
@@ -37,6 +39,7 @@ app.get("/posts/new",(req,res)=>{
     res.render("new_post.ejs");
 });
 
+//new post added to posts route
 app.post("/posts",(req,res)=>{
     let {username,content}=req.body;
     let id=uuidv4();
@@ -44,11 +47,27 @@ app.post("/posts",(req,res)=>{
     res.redirect("/posts");
 });
 
+//show invidual posts in detail
 app.get("/posts/:id",(req,res)=>{
     let {id}=req.params;
     let post=posts.find((p)=>id===p.id);
-    res.render("show.ejs",{ post });
+    res.render("show.ejs",{post});
 });
+
+app.patch("/posts/:id",(req,res)=>{
+   let { id }=req.params;
+   let newContent=req.body.content;
+   let post=posts.find((p)=> id === p.id);
+   post.content=newContent;
+   res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>id === p.id);
+    res.render("edit_form.ejs",{ post });
+});
+
 
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`);
